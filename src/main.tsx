5167f78338a397ecf390f30e6097b47256c49ba7
Roman <sselbstmord@gmail.com>
2025-03-19 10:21:04 +0300

    fix v 10000000

--- src/main.tsx
+++ src/main.tsx
@@ -9,9 +9,9 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
-            <BrowserRouter>
+            <HashRouter>
                 <App />
-            </BrowserRouter>
+            </HashRouter>
         </QueryClientProvider>
     </StrictMode>,
 );
